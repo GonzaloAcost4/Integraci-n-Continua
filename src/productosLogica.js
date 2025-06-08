483c5086965b505d5969cd2560b6338e1490cf5b
Gonzalo Acosta <93352410+GonzaloAcost4@users.noreply.github.com>
2025-06-08 19:08:55 -0300

    Update productosLogica.js

--- src/productosLogica.js
+++ src/productosLogica.js
@@ -1,5 +1,5 @@
 export const productos = [
-    { nombre: 'Pantalón', precio: 2000 },
+    { nombre: 'Pantalón', precio: 20000 },
     { nombre: 'Abrigo', precio: 200 },
     { nombre: 'Camisa', precio: 50 },
     { nombre: 'Zapatos', precio: 1000 },
