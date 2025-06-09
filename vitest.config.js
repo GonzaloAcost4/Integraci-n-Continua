0bef6df2b607c307963191be2e80cfc48920ce27
Gonzalo Acosta <gonzaloacosta258@gmail.com>
2025-06-08 21:20:14 -0300

    coveragee

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,9 @@
-// vitest.config.js
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    coverage: {
-      reporter: ['text', 'lcov'],
-      reportsDirectory: 'coverage'
-    }
-  }
+    test: {
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+        },
+    },
 })
